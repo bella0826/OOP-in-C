bf0518be856464a3f691cb92bad93f2bf73bb85d
yeehan <bellaa9677@gmail.com>
2022-12-16 15:57:56 +0800

    added on 12/16

---
+++ Shapes.r
@@ -0,0 +1,18 @@
+#ifndef SHAPES_R
+#define SHAPES_R
+
+#include "Point.r"
+
+struct Circle{
+	const struct Point _;
+	int rad;
+};
+
+struct Rectangle{
+	const void * class;
+	struct Point *Point;
+	int width;
+	int length;
+};
+
+#endif
